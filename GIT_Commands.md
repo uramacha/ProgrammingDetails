0452e1f06dcc72499fcf8eede3f375c8890fdef1
Udayakumar <udayakumarjp@gmail.com>
2024-01-01 20:30:39 +0530

    first sample project updated

---
+++ GIT_Commands.md
@@ -0,0 +1,5 @@
+
+git status - check all added files
+git commit - 
+git diff -
+git add - 
---
+++ JetPackComp/app/src/main/java/com/example/jetpackcomp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.jetpackcomp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackcomp.ui.theme.JetPackCompTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetPackCompTheme {
+        Greeting("Android")
+    }
+}
